ff2c2ea0fb2c96faa7a232a94c559a0763a6750d
suwarna-wave <suwarnapyakurel5@gmail.com>
2025-01-21 10:39:52 +0545

    selection sort

---
+++ selection.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void selection_sort(int arr[100001], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+
+
+//display function
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+
+//main function
+int main() {
+    int n;
+    clock_t start, end;
+    double cpu_time_used;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    srand(time(0));
+    
+    // Generate random array
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+    
+    printf("Original array: ");
+    printArray(arr, n);
+    
+    // Start time measurement
+    start = clock();
+    
+    // Sort array calling selection sort
+    selection_sort(arr, n);
+    
+    // End time measurement
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    
+    printf("Sorted array: ");
+    printArray(arr, n);
+    printf("Time taken: %f seconds\n", cpu_time_used);
+    
+    return 0;
+}
